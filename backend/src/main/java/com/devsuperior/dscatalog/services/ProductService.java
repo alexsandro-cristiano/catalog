5dd268ba2fc7925710c8bf340c5dc3f053f1e200
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-03-28 14:11:00 -0300

    refatorando product service

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundExceptions;
@@ -23,6 +26,8 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> getAllPage(PageRequest pageRequest) {
@@ -40,12 +45,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product product = new Product();
-		product.setName(dto.getName());
-		product.setDescription(dto.getDescription());
-		product.setPrice(dto.getPrice());
-		product.setImgUrl(dto.getImgUrl());
-		product.setDate(dto.getDate());
-		// como fica a coleção ?
+		copyDtoToEntity(dto, product);
 		product = repository.save(product);
 		return new ProductDTO(product);
 	}
@@ -54,11 +54,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product product = repository.getOne(id);
-			product.setName(dto.getName());
-			product.setDescription(dto.getDescription());
-			product.setPrice(dto.getPrice());
-			product.setImgUrl(dto.getImgUrl());
-			product.setDate(dto.getDate());
+			copyDtoToEntity(dto, product);
 			product = repository.save(product);
 			return new ProductDTO(product);
 		} catch (EntityNotFoundException e) {
@@ -73,6 +69,21 @@ public class ProductService {
 			throw new ResourceNotFoundExceptions("Id not found " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
+		}
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product product) {
+		product.setName(dto.getName());
+		product.setDescription(dto.getDescription());
+		product.setPrice(dto.getPrice());
+		product.setImgUrl(dto.getImgUrl());
+		product.setDate(dto.getDate());
+
+		// como fica a coleção ?
+		product.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			product.getCategories().add(category);
 		}
 	}
 
